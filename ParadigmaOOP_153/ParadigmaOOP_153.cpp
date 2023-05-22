089a2c32294888ca9b5d2d9c0b62b73ffe7dee20
azmianshari <azmianshari123@gmail.com>
2023-05-22 15:21:52 +0700

    Add project files.

---
+++ ParadigmaOOP_153/ParadigmaOOP_153.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float;
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai)
+};
