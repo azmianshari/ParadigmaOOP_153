8f45a9242af1f4322be248deb897879096b342d0
azmianshari <azmianshari123@gmail.com>
2023-05-22 18:26:23 +0700

    Membuat kelas dokter dan pasien

---
+++ Asosiasi/Asosiasi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+class pasien {
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\ Ada \n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama << "\ Tidak ada \n";
+	}
+	void tambahDokter(dokter*);
+	
+};
